eaf26550bc7781f24d366d0e459bcda1c8862dfb
Luciano <129859436+LucianoColalunga@users.noreply.github.com>
2024-07-12 19:19:29 -0300

    Iniciando con pokedex
    Se incorporo Jquery y creacion de un maquetado basico para la pokedex

---
+++ index.js
@@ -0,0 +1,15 @@
+const URL = 'https://pokeapi.co/api/v2/pokemon/';
+const Input = document.querySelector('pokemon');
+const pokedexSection = document.querySelector('pokedex');
+
+
+fetch("https://pokeapi.co/api/v2/1/")
+.then(respuesta=>respuesta.json())
+.then(respuestaJSON => {
+
+    
+
+    
+    
+})
+console.log(respuestaJSON);
